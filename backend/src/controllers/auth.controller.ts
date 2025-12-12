05a076c11d22d3ce7d9cd5e0ea2dd078ecdbf422
ankit <ajha45075@gmail.com>
2025-12-12 22:48:12 +0530

    login controller done

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -11,7 +11,7 @@ export const register = async (req: Request, res: Response) => {
     if (existedUser) {
       return res
         .status(409)
-        .json({ message: "Email already exists.", success: false });
+        .json({ message: "Please use different email.", success: false });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -28,19 +28,22 @@ export const register = async (req: Request, res: Response) => {
       _id: newUser._id,
       name: newUser.name,
       email: newUser.email,
+      role:newUser.role
     };
 
-    res.cookie("auth", token, {
-      httpOnly: true,
-      secure: true,
-      sameSite: "strict",
-      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milisecond
-    }).status(201).json({
-      message: "User registered successfully",
-      success: true,
-      user: userResponse,
-    });
-    
+    res
+      .cookie("auth", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milisecond
+      })
+      .status(201)
+      .json({
+        message: "User registered successfully",
+        success: true,
+        user: userResponse,
+      });
   } catch (error) {
     res.status(500).json({ error: "Internal server error.", success: false });
   }
@@ -48,7 +51,48 @@ export const register = async (req: Request, res: Response) => {
 
 export const login = async (req: Request, res: Response) => {
   try {
+    const { email, password } = await req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ message: "User not found.", success: false });
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword) {
+      return res
+        .status(404)
+        .json({ message: "Wrong email or password.", success: false });
+    }
+
+  
+    const token = generateToken(user);
+
+    res
+      .cookie("auth", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milisecond
+      })
+      .status(201)
+      .json({
+        message: "User registered successfully",
+        success: true,
+        user: {
+          _id:user._id, 
+          name:user.name,
+          email:user.email,
+          role:user.role
+        },
+      });
   } catch (error) {
     res.status(500).json({ error: "Internal server error.", success: false });
   }
 };
+
+
--- backend/src/utils/validateRegister.ts
+++ backend/src/utils/validateRegister.ts
@@ -14,10 +14,13 @@ export function validateRegister(
       .json({ message: "All fields are required.", success: false });
   }
 
-  if(name.trim().length<3){
+  if (name.trim().length < 3) {
     return res
       .status(400)
-      .json({ message: "Name should be atleast 3 characters long.", success: false });
+      .json({
+        message: "Name should be atleast 3 characters long.",
+        success: false,
+      });
   }
 
   if (!validator.isEmail(email)) {
@@ -26,18 +29,26 @@ export function validateRegister(
       .json({ message: "Please enter valid email address.", success: false });
   }
 
+  // Check for spaces
+  if (/\s/.test(password)) {
+    return res.status(400).json({
+      message: "Password cannot contain spaces.",
+      success: false,
+    });
+  }
+
   const isValidPassword = validator.isStrongPassword(password, {
     minLength: 6,
     minUppercase: 1,
     minNumbers: 1,
     minSymbols: 0,
-  }); 
+  });
 
-  if(!isValidPassword){
-      return res
+  if (!isValidPassword) {
+    return res
       .status(400)
-      .json({ message: "Please enter strong email address.", success: false });
+      .json({ message: "Please enter strong password.", success: false });
   }
 
-  next(); 
+  next();
 }
