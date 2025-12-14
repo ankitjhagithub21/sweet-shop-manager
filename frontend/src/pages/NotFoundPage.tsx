022e8bd4d3ad595beeb7f9001e2ae453eaeb0530
ankit <ajha45075@gmail.com>
2025-12-14 18:10:58 +0530

    updated not found page

--- frontend/src/pages/NotFoundPage.tsx
+++ frontend/src/pages/NotFoundPage.tsx
@@ -5,10 +5,10 @@ const NotFoundPage = () => {
   return (
     <div className="hero bg-base-200 min-h-screen">
   <div className="hero-content text-center">
-    <div className="max-w-md">
+    <div className="flex flex-col gap-10">
       <h1 className="text-5xl font-bold">Oops, Page Not found.</h1>
       
-      <Link to={"/"} className="btn btn-primary">Back to Home</Link>
+      <Link to={"/"} className="btn btn-secondary w-fit mx-auto">Back to Home</Link>
     </div>
   </div>
 </div>
